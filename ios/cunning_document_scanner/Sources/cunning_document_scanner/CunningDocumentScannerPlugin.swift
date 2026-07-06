b3e95b84b65b5a717ab16640a744f4a160bdea98
rosenberg_ptr <rosenberg.ptr@gmail.com>
2026-07-06 14:13:57 +0200

    SPM migration

---
+++ ios/cunning_document_scanner/Sources/cunning_document_scanner/CunningDocumentScannerPlugin.swift
@@ -0,0 +1,196 @@
+import Flutter
+import UIKit
+import Vision
+import VisionKit
+
+@available(iOS 13.0, *)
+public class CunningDocumentScannerPlugin: NSObject, FlutterPlugin,
+    VNDocumentCameraViewControllerDelegate {
+    var resultChannel: FlutterResult?
+    var presentingController: VNDocumentCameraViewController?
+    var maxPageCount: Int = 100
+    let outputMaxDimension: CGFloat = 2048
+    let outputJpegQuality: CGFloat = 0.82
+    let outputQueue = DispatchQueue(
+        label: "biz.cunning.cunning_document_scanner.output",
+        qos: .userInitiated
+    )
+
+    public static func register(with registrar: FlutterPluginRegistrar) {
+        let channel = FlutterMethodChannel(
+            name: "cunning_document_scanner",
+            binaryMessenger: registrar.messenger()
+        )
+        let instance = CunningDocumentScannerPlugin()
+        registrar.addMethodCallDelegate(instance, channel: channel)
+    }
+
+    public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+        guard call.method == "getPictures" else {
+            result(FlutterMethodNotImplemented)
+            return
+        }
+
+        let arguments = call.arguments as? [String: Any]
+        let rawPageCount = arguments?["noOfPages"]
+        let requestedPageCount = rawPageCount as? Int
+            ?? (rawPageCount as? NSNumber)?.intValue
+            ?? 100
+        maxPageCount = max(1, requestedPageCount)
+
+        guard let presentedVC = rootViewController else {
+            result(
+                FlutterError(
+                    code: "NO_VIEW_CONTROLLER",
+                    message: "No root view controller available",
+                    details: nil
+                )
+            )
+            return
+        }
+
+        resultChannel = result
+        if VNDocumentCameraViewController.isSupported {
+            presentingController = VNDocumentCameraViewController()
+            if let presentingController = presentingController {
+                presentingController.delegate = self
+                presentedVC.present(presentingController, animated: true)
+            } else {
+                result(
+                    FlutterError(
+                        code: "ERROR",
+                        message: "Failed to initialize document camera",
+                        details: nil
+                    )
+                )
+            }
+        } else {
+            result(
+                FlutterError(
+                    code: "UNAVAILABLE",
+                    message: "Document camera is not available on this device",
+                    details: nil
+                )
+            )
+        }
+    }
+
+    var rootViewController: UIViewController? {
+        let keyWindow = UIApplication.shared.connectedScenes
+            .compactMap { $0 as? UIWindowScene }
+            .flatMap { $0.windows }
+            .first { $0.isKeyWindow }
+        return keyWindow?.rootViewController
+    }
+
+    func getDocumentsDirectory() -> URL {
+        FileManager.default.urls(
+            for: .documentDirectory,
+            in: .userDomainMask
+        ).first ?? FileManager.default.temporaryDirectory
+    }
+
+    func boundedImage(_ image: UIImage) -> UIImage {
+        let width = image.size.width
+        let height = image.size.height
+        let longestSide = max(width, height)
+        if longestSide <= outputMaxDimension {
+            return image
+        }
+
+        let scale = outputMaxDimension / longestSide
+        let targetSize = CGSize(width: width * scale, height: height * scale)
+        let format = UIGraphicsImageRendererFormat.default()
+        format.scale = 1
+        let renderer = UIGraphicsImageRenderer(size: targetSize, format: format)
+        return renderer.image { _ in
+            image.draw(in: CGRect(origin: .zero, size: targetSize))
+        }
+    }
+
+    public func documentCameraViewController(
+        _ controller: VNDocumentCameraViewController,
+        didFinishWith scan: VNDocumentCameraScan
+    ) {
+        let tempDirPath = getDocumentsDirectory()
+        let currentDateTime = Date()
+        let df = DateFormatter()
+        df.dateFormat = "yyyyMMdd-HHmmss"
+        let formattedDate = df.string(from: currentDateTime)
+        let pageCount = min(scan.pageCount, maxPageCount)
+        let result = resultChannel
+        let controllerToDismiss = presentingController ?? controller
+        resultChannel = nil
+        presentingController = nil
+        controllerToDismiss.dismiss(animated: true)
+
+        outputQueue.async { [self] in
+            var filenames: [String] = []
+            var flutterError: FlutterError?
+            for i in 0 ..< pageCount {
+                autoreleasepool {
+                    let page = boundedImage(scan.imageOfPage(at: i))
+                    let url = tempDirPath.appendingPathComponent(
+                        formattedDate + "-\(i).jpg"
+                    )
+                    guard let imageData = page.jpegData(
+                        compressionQuality: outputJpegQuality
+                    ) else {
+                        flutterError = FlutterError(
+                            code: "IMAGE_ENCODING_ERROR",
+                            message: "Failed to encode image",
+                            details: nil
+                        )
+                        return
+                    }
+                    do {
+                        try imageData.write(to: url)
+                        filenames.append(url.path)
+                    } catch {
+                        flutterError = FlutterError(
+                            code: "FILE_WRITE_ERROR",
+                            message: "Failed to write file: \(error.localizedDescription)",
+                            details: nil
+                        )
+                    }
+                }
+                if flutterError != nil {
+                    break
+                }
+            }
+
+            DispatchQueue.main.async {
+                if let flutterError = flutterError {
+                    result?(flutterError)
+                } else {
+                    result?(filenames)
+                }
+            }
+        }
+    }
+
+    public func documentCameraViewControllerDidCancel(
+        _ controller: VNDocumentCameraViewController
+    ) {
+        resultChannel?(nil)
+        resultChannel = nil
+        presentingController?.dismiss(animated: true)
+        presentingController = nil
+    }
+
+    public func documentCameraViewController(
+        _ controller: VNDocumentCameraViewController,
+        didFailWithError error: Error
+    ) {
+        resultChannel?(
+            FlutterError(
+                code: "ERROR",
+                message: error.localizedDescription,
+                details: nil
+            )
+        )
+        resultChannel = nil
+        presentingController?.dismiss(animated: true)
+        presentingController = nil
+    }
+}
